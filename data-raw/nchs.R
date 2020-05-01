2b45ab8b800b9bf2c1ade5c4ccdfa162f8f97de1
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2020-05-01 16:50:04 -0400

    Add svy_freq() function

--- data-raw/nchs.R
+++ data-raw/nchs.R
@@ -7,10 +7,44 @@ load("R:/OCPH/EPI/BHSDM/Group/BRFSS/2017/Final/WorkingDataFile/nhbrfs17_working.
 
 nh_svy <- as_survey_design(nh17brfs, strata = X.STSTR, weights = X.LLCPWT)
 
+valid_n <- function(x) unweighted(length(na.omit(x)))
+# nh_svy %>% summarize(n = valid_n(DIABETE3))
+# 
+# nh_svy %>% 
+#   summarize(n = valid_n(DIABETE3))
+
+svy_freq <- function(design, grp, svyvar, outcome) {
+  grp_var <- sym(grp)
+  ind_var <- sym(svyvar)
+  design %>%
+    group_by(!!grp_var) %>% 
+    summarize(n = unweighted(length(na.omit(!!ind_var))), # drops NA in indicator
+              pct = survey_mean(!!ind_var == outcome,
+                                vartype = c("var", "ci"), na.rm = TRUE, 
+                                proportion = TRUE, prop_method = "beta")) %>% 
+    drop_na() %>% # drop NA in group
+    mutate(n_eff = pct * (1 - pct) / pct_var,
+           ci_width = pct_upp - pct_low,
+           ci_rel_width = ci_width / pct) %>% 
+    # mutate_at(c(vars(starts_with("pct"), "ci_width")),
+    #           function(x) round(100 * x, 1)) %>% 
+    rename_at(vars(grp), ~ "Population") %>% 
+    # add indicator as column
+    mutate(indicator = outcome) %>% 
+    select(indicator, Population, n, n_eff, everything(), -pct_var) 
+  }
+
+
+
+svy_freq(nh_svy, "SEX", "DIABETE3", "Told have diabetes")
+# use near() to determine if zero events--pct should be 0 or 1
+
 # proportion
 svy_prop <- nh_svy %>% 
-  summarize(pct = survey_mean(DIABETE3 == "Told have diabetes", na.rm = TRUE,
-                              vartype = "ci", proportion = TRUE, 
+  group_by(SEX) %>% 
+  summarize(n = unweighted(length(na.omit(SEX, DIABETE3))),
+            pct = survey_mean(DIABETE3 == "Told have diabetes", na.rm = TRUE,
+                              vartype = c("var", "ci"), proportion = TRUE, 
                               prop_method = "beta"))
 ## compute CI length and relative CI 
 
@@ -22,7 +56,7 @@ svy_ttl <- nh_svy %>%
 # unweighted sample size; remove missings
 nh_svy %>% 
   group_by(DIABETE3) %>% 
-  summarize(n = na.omit(unweighted(n()))) %>% 
+  summarize(n = na.omit(length(na.omitunweighted(n()))) %>% 
   summarize(n = sum(n, na.rm = TRUE))
   
 # degrees of freedom
