7629558d506685a4b2d5455e89952caac8a2849b
Laviolette, Michael <michael.laviolette@dhhs.nh.gov>
2020-05-01 14:10:35 -0400

    Initial commit

---
+++ nchs-logic.R
@@ -0,0 +1,45 @@
+# logic for NCHS standards
+
+# compute sample size n
+if (n < 30) {
+  # Suppress
+  # return()
+  } else {
+    # check the confint
+    }
+
+# check the confint
+# compute confint abs_length
+if (abs_length > 0.3) {
+  # Suppress
+  # return() 
+  } else {
+      # check the relative width
+    }
+  
+# check the relative width
+# compute confint rel_length
+if (rel_width > 1.3) {
+  # Suppress
+  # return() 
+  } else {
+    # check for zero events
+    }
+
+# check for zero events
+if (events == 0) {
+  # Review
+  # return()
+  } else {
+    # check survey degf
+    }
+
+# check survey degf
+if (degf < 8) {
+  # Review
+  } else {
+    # Present
+  }
+
+
+
